31618fe85d57e6476476e7641e375f867f122e8c
Cassie/Ren <cassandra.chen@uoit.net>
2022-01-18 16:16:58 -0500

    button functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,44 @@
 import React from "react";
+import classNames from "classnames";
+
+// import { useState } from "react";
+// import { storiesOf } from '@storybook/react'
 
 import "components/Button.scss";
+import "components/DayListItem"
+
+// storiesOf('Button')
+//    .add('Default Button', () => {})
+//    .add()
+//    .add()
+//    .add()
+
+
+// export default function Button(props) {
+//    const [pressed, setPressed] = useState('Default');
+//    const click = () => {setPressed((pressed))}
+//    return (
+//    <>
+//       <button>
+//          {pressed}
+//       </button>
+//    </>);
+// }
 
 export default function Button(props) {
-   return <></>;
-}
+   
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+    });
+
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,60 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames("day-list__item", {
+    "--selected": props.selected,
+    "--full": props.spots === 0
+  });
+
+  const formatSpots = (props) => {
+    return props.spots
+
+  }
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={ dayClass }>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots} spots remaining</h3>
+    </li>
+  );
+}
+
+
+// export default function DayListItem(props) {
+
+//   const dayPlan = classNames('text', {
+
+//     "text--unselected": props.unselected,
+//     "text--selected": props.selected,
+//     "text--full": props.full,
+//     "text--clickable": props.clickable
+
+//   });
+
+  // const whatDay = classNames(props, {
+  //   "Monday": props.monday,
+  //   "Tuesday": props.tuesday,
+  //   "Wednesday": props.wednesday,
+  //   "Thursday": props.thursday,
+  //   "Friday": props.friday,
+  //   "Saturday": props.saturday,
+  //   "Sunday": props.sunday
+  // })
+
+//   return (
+//     <li>
+//       <h2 className={ dayPlan }>{ whatDay }</h2> 
+//       <h3 className={ dayPlan }>{ props.spots }</h3>
+//       <button onClick={ whatDay }>Select day of the week</button>
+//       <p></p>
+//     </li>
+//   );
+// }
+
+// The <li> represents the entire day item
+// The <h2> should display the day name
+// The <h3> should display the spots remaining for a day
