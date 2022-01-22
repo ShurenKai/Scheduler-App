a9e2da09782fda72798163094698aa1eaa101202
Cassie/Ren <cassandra.chen@uoit.net>
2022-01-21 23:44:32 -0500

    finally get tests to work! build more helper functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,57 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointments";
 import "components/Appointments";
-import { getAppointmentsForDay } from "./helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "./helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {
-      interview: null,
+      id: null,
       time: "",
+      interview: {
+        id: null,
+        name: "",
+        avatar: "",
+      },
     },
   });
   const setDay = (day) => setState({ ...state, day });
-  const dailyAppointments = getAppointmentsForDay(
-    state.appointments,
-    state.day
-  );
-  const apps = dailyAppointments.map((app) => (
-    <Appointment key={app.id} {...app} />
-  ));
 
-  Promise.all([
-    axios.get("http://localhost:8001/api/days"),
-    axios.get("http://localhost:8001/api/appointments"),
-    axios.get("http://localhost:8001/api/interviewers"),
-  ]).then((all) => {
-    setState({ day: "Monday", days: all[0].data });
-    setState({ id: all[1].data });
-    setState({ dailyAppointments: all[2].data });
-  });
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers"),
+    ])
+      .then((all) => {
+        console.log(all);
+        setState({
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data,
+        });
+      })
+      .catch((err) => {
+        console.log("ya dun goofed: ", err);
+      });
+  }, []);
 
-  Promise.all([
-    Promise.resolve("first"),
-    Promise.resolve("second"),
-    Promise.resolve("third"),
-  ]).then((all) => {
-    setState((prev) => ({
-      ...prev,
-      first: all[0],
-      second: all[1],
-      third: all[2],
-    }));
-  });
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  console.log(dailyAppointments);
+  const interview = getInterview(state, state.appointments.interview);
 
-  // .catch((all) => {
-  //   console.log("Rest in state", all[0].data);
-  //   console.log("Rest in appointment", all[1].err);
-  // });
+  console.log(interview);
 
   return (
     <main className="layout">
@@ -74,9 +69,18 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {apps}
+        {dailyAppointments.map((dail) => (
+          <Appointment
+            key={dail.id}
+            id={dail.id}
+            time={dail.time}
+            interview={dail.interview}
+          />
+        ))}
         <Appointment key="last" time="5PM" />
       </section>
     </main>
   );
 }
+
+// {(setDay={setDay})}
--- src/components/helpers/selectors.js
+++ src/components/helpers/selectors.js
@@ -1,4 +1,37 @@
-export function getAppointmentsForDay(state, day) {
-  const filterDays = state.days.filter((days) => days.name === day);
-  return filterDays;
+function getAppointmentsForDay(state, day) {
+  let filt = state.days.filter((x) => x.name === day);
+  return filt.length === 0
+    ? []
+    : filt[0].appointments.map((app) => state.appointments[app]);
 }
+
+function getInterview(state, interview) {
+  if (interview == null) {
+    return null;
+  }
+  let values = Object.values(state.appointments);
+  let result;
+
+  for (let val of values) {
+    if (val.interview == null) {
+      result = null;
+    } else if (val.interview !== null) {
+      if (val.interview.interviewer === interview.interviewer) {
+        val.interview.interviewer = state.interviewers[interview.interviewer];
+        return val.interview;
+      } else if (
+        (val.interview.interviewer !== interview.interviewer) &
+        (val.id === values.length)
+      ) {
+        return null;
+      } else {
+        continue;
+      }
+    }
+  }
+  return result;
+}
+
+export { getAppointmentsForDay, getInterview };
+
+// val = { id: 1, time: '12pm', interview: null }
