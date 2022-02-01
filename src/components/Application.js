0555809e3a3ac943bba2dff58f849f6687c4fa3c
Cassie/Ren <cassandra.chen@uoit.net>
2022-01-31 19:38:04 -0500

    move promise all to hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,24 +20,6 @@ export default function Application() {
   const { state, setState, setDay, bookInterview, cancelInterview } =
     useApplicationData();
 
-  useEffect(() => {
-    Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers"),
-    ])
-      .then((all) => {
-        setState({
-          days: all[0].data,
-          appointments: all[1].data,
-          interviewers: all[2].data,
-        });
-      })
-      .catch((err) => {
-        console.log("ya dun goofed: ", err);
-      });
-  }, [setState]);
-
   ///////////////////////////////////////////
   // Where helper functions are being used //
   ///////////////////////////////////////////
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 //////////////////////////////////////////////////////////////////
@@ -21,6 +21,28 @@ export default function useApplicationData() {
     },
     interviewers: {},
   });
+
+  //////////////////////////////////////////////////////
+  // Acquisition of data for interviewer appointments //
+  //////////////////////////////////////////////////////
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers"),
+    ])
+      .then((all) => {
+        setState({
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data,
+        });
+      })
+      .catch((err) => {
+        console.log("ya dun goofed: ", err);
+      });
+  }, [setState]);
+
   const setDay = (day) => {
     setState({ ...state, day });
   };
